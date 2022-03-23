fc4d2d367c3a9591b33d19e1a191fb88e89d6a43
giuli <giuli@172.21.103.77>
2022-03-23 18:18:45 +0100

    programma finito primo punto

--- src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -1,25 +1,94 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.spellchecker;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.spellchecker.model.Dictionary;
+import it.polito.tdp.spellchecker.model.RichWord;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import java.util.*;
 
 public class FXMLController {
+	
+	private Dictionary model;
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
+    @FXML
+    private ComboBox<String> cmbLingua;
+
+    @FXML
+    private TextArea txtErrori;
+
+    @FXML
+    private Label txtNErrori;
+
+    @FXML
+    private TextArea txtSpelling;
+
+    @FXML
+    private Label txtTempo;
+
+    @FXML
+    void doSpellCheck(ActionEvent event) { 
+    	List<String> frase=new LinkedList<>();
+    	String inserito= txtSpelling.getText().toLowerCase();
+    	String ins=inserito.replaceAll("[.,\\/#!$%\\^&\\*;:{}=\\-_`~()\\[\\]\"]", "");
+    	System.out.print(ins);
+    	String array[]=ins.split(" ");
+    	for(String ss:array) {
+    		frase.add(ss);    		
+    	}
+    	List<RichWord> pRW= new LinkedList<>();
+    	pRW=model.spellCheckText(frase);
+    	
+    	for(RichWord rr: pRW) {
+    		txtErrori.appendText(rr.toString()+"\n");
+    	}
+    	
+    	
+    	//StringTokenizer st=new StringTokenizer(txtSpelling.getText()," ");
+    	
+    	
+    	
 
     }
-}
 
+    @FXML
+    void doTextClear(ActionEvent event) {
+
+    }
+    
+    @FXML
+    void handleCmbLanguage(ActionEvent event) {
+    	String lingua=cmbLingua.getValue();
+    	model.loadDictionary(lingua);
+
+    }
+    
+    void setModel(Dictionary model) {
+    	this.model=model;    	
+    }
+
+    @FXML
+    void initialize() {
+        assert cmbLingua != null : "fx:id=\"cmbLingua\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtErrori != null : "fx:id=\"txtErrori\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtNErrori != null : "fx:id=\"txtNErrori\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtSpelling != null : "fx:id=\"txtSpelling\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtTempo != null : "fx:id=\"txtTempo\" was not injected: check your FXML file 'Scene.fxml'.";
+        cmbLingua.getItems().clear();
+        cmbLingua.getItems().addAll("English","Italian");
+
+    }
+
+}
 
